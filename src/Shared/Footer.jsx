967ca6167f6e9a7c5c586af641f97e961f47e529
Rohan <rohan26ir@gmail.com>
2025-03-03 20:46:25 +0600

    responsible foooter

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -10,14 +10,14 @@ const Footer = () => {
         <div className='flex flex-col md:flex-row justify-between items-center bg-gray-800 text-white p-5'>
           
           {/* section */}
-          <div className=''>
+          <div className='w-[50%]  md:w-[100%]'>
             <img src="/public/diary.png" alt="" className='h-14 ' />
             <h2 className='text-xl font-bold'>Dev Diary24</h2>
           </div>
           {/* section */}
-          <div className=''>
+          <div className='hidden md:block  w-[100%]'>
 
-            <div>
+            <div className=''>
               <h2 className='text-xl font-bold'>Contact</h2>
               <ul>
                 <li>Phone: 01869396022</li>
@@ -28,15 +28,21 @@ const Footer = () => {
           </div>
           {/* section */}
           <div className=''>
-            <h2 className='text-xl font-bold'>Follow Me</h2>
-            
-  <div className='flex gap-2 text-4xl'>
-   <a href=""> <FaSquareGithub /> </a>
-   <a href=""> <FaLinkedin /> </a>
-   <a href=""> <RiPassportFill /> </a>
-   <a href=""> <FaSquareFacebook /> </a>
-  </div>
 
+            <div>
+            
+            <div className='hidden md:block'>
+            <h2 className='text-xl font-bold'>Follow Me</h2>
+            </div>
+            
+            <div className='flex gap-2 text-4xl'>
+             <a href=""> <FaSquareGithub /> </a>
+             <a href=""> <FaLinkedin /> </a>
+             <a href=""> <RiPassportFill /> </a>
+             <a href=""> <FaSquareFacebook /> </a>
+            </div>
+          
+            </div>
           </div>
 
 
